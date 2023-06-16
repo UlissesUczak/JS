fd9e6ae9a4719cb0fa9ebe198db6ebca85e31d30
Ulisses U <uczakulisses@gmail.com>
2023-06-16 16:24:24 -0300

    logica JS

---
+++ ex005/index.js
@@ -0,0 +1,9 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+console.log(`Se beber, não dirija`)
+
+if (vel <=60.5) {
+    console.log('Dirija com cuidado')
+} else {
+    console.log(`sua velocidade foi de ${vel} você foi multado`)
+}
