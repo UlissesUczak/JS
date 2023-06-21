c02d13fb84ec431b76e35e9ac79120077f43d442
Ulisses U <uczakulisses@gmail.com>
2023-06-21 17:35:07 -0300

    switch

---
+++ ex006/switch.js
@@ -0,0 +1,24 @@
+switch (diaSem) {
+  case 1:
+    console.log("10");
+    break
+  case 2:
+    console.log("20");
+    break
+  case 3:
+    console.log("30");
+    break
+  case 4:
+    console.log("40");
+    break
+  case 5:
+    console.log("50");
+    break
+  case 6:
+    console.log("60");
+    break
+  case 7:
+    console.log("70");
+    break
+    default:
+}
