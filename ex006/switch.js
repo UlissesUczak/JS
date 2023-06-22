34701a3cd6ec1bc77afc026175d62c37df01c0c2
Ulisses U <uczakulisses@gmail.com>
2023-06-22 11:17:37 -0300

    switch testes

--- ex006/switch.js
+++ ex006/switch.js
@@ -1,24 +1,11 @@
-switch (diaSem) {
-  case 1:
-    console.log("10");
+var agora = new Date()
+var diaSem = agora.getDay()
+
+switch(diaSem) {
+  case 0:
+    console.log('Domingo')
     break
-  case 2:
-    console.log("20");
-    break
-  case 3:
-    console.log("30");
-    break
-  case 4:
-    console.log("40");
-    break
-  case 5:
-    console.log("50");
-    break
-  case 6:
-    console.log("60");
-    break
-  case 7:
-    console.log("70");
-    break
-    default:
+    case 1:
+      console.log('Segunda')
+      
 }
