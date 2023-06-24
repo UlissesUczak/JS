89de64069ab8586c1dc4a555cab384710320b1cd
Ulisses U <uczakulisses@gmail.com>
2023-06-24 15:01:04 -0300

    correções feitas e exercicios finalizados

--- exercicios-praticos/ex002/script.js
+++ exercicios-praticos/ex002/script.js
@@ -6,7 +6,47 @@ function verificar() {
   if (fano.value.length == 0 || Number(fano.value) > ano) {
     window.alert("[ERRO!] Verifique os dados e tente novamente");
   } else {
-    var fsex = document.getElementByName("radsex");
-    var idade = ano - Number(fano.value);
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fano.value)
+    var genero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    if (fsex[0].checked) {
+      genero = 'Homem'
+      if (idade >= 0 && idade < 10) {
+        //criança
+        img.setAttribute('src', 'criancam.jpg')
+      } else if (idade >=10 && idade < 21) {
+        //Jovem
+        img.setAttribute('src', 'jovemm.jpg')
+      } else if (idade < 50) {
+        //Adulto
+        img.setAttribute('src', 'adultom.jpg')
+      } else {
+        //Idoso
+        img.setAttribute('src', 'idosom.jpg')
+      }
+    } else if (fsex[1].checked) {
+      genero = 'Mulher'
+      if (idade >= 0 && idade < 10) {
+        //criança
+        img.setAttribute('src', 'criancaf.jpg')
+      } else if (idade >=10 && idade < 21) {
+        //Jovem
+        img.setAttribute('src', 'jovemf.jpg')
+      } else if (idade < 50) {
+        //Adulto
+        img.setAttribute('src', 'adultof.jpg')
+      } else {
+        //Idoso
+        img.setAttribute('src', 'idosof.jpg')
+      }
+    }
+    res.style.textAlign = 'center'
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+    res.appendChild(img)
+  
   }
 }
+
+
