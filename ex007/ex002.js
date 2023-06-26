564dcab90e794ec7381d7b9ca0a3f6a6256adbcf
Ulisses U <uczakulisses@gmail.com>
2023-06-26 09:38:13 -0300

    done

---
+++ ex007/ex002.js
@@ -0,0 +1,32 @@
+//WHILE ABAIXO!
+
+
+/*var c = 1
+while (c<=5) {
+    console.log(c)
+    c++
+}
+*/
+//----------------------
+//DO WHILE ABAIXO!
+
+/*var c = 1
+do {
+    console.log(c)
+    c++
+} while (c <=5)
+*/
+
+//-----------------------
+
+for (var c = 1;c<=5;c++) {
+    console.log(c)
+
+
+
+
+}
+
+
+
+
